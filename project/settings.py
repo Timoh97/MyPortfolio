cf212d6293770def3b2a9c4636450d7d1ae549d2
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-17 03:37:00 +0300

    add secret key and mode configuration in settings.py file

--- project/settings.py
+++ project/settings.py
@@ -83,6 +83,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
+SECRET_KEY = config('SECRET_KEY')
+MODE=config("MODE", default="dev")
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
