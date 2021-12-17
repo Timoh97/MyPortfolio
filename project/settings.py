672d1512e2056ba54d7686e6bea0535017d108c0
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-17 03:35:39 +0300

    add whitenoise to the middleware category in settings.py

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-DEBUG = True
 
 
 # Application definition
@@ -59,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
